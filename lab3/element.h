1cb8e4ab8595ffef5bdc376a49dc7acd1c05d7e7
Virendrasinh Mane <virendra@Virendrasinhs-MacBook-Air.local>
2024-01-28 18:01:16 +0530

    Implemented stack with array

---
+++ lab3/element.h
@@ -0,0 +1,14 @@
+#ifndef ELEMENT_H
+#define ELEMENT_H
+
+//Definition for the element of the stack
+struct Element
+{
+    int int_value;
+    float float_value;
+};
+
+typedef struct Element Element;
+
+
+#endif 
---
+++ lab3/stack/array/stack.c
@@ -0,0 +1,63 @@
+#include "stack.h"
+#include "element.h"
+#include <stdio.h>
+#include <stdlib.h>
+#define STACK_SIZE 1000
+//Defines the maxmimum size of the stack
+
+struct Stack
+{
+    int top;
+    Element data[STACK_SIZE];
+};
+
+Stack* newStack(){
+    Stack* s = (Stack*)malloc(sizeof(Stack));
+
+    if(s != NULL)
+    {
+        s->top = -1;
+    }
+    return s;
+}
+
+bool push(Stack * s,Element e){
+    if(s->top == STACK_SIZE - 1){
+        return false;
+    }
+    s->data[++(s->top)] = e;
+    return true;
+}
+
+Element* top(Stack* stack){
+    Element* res = (Element*)malloc(sizeof(Element));
+    if(stack->top == -1){
+        return res;
+    }
+    //The -> deferences memory can be used to directly  access members
+    //the members of a struct
+    *res = stack->data[stack->top];
+
+    return res;
+}
+
+bool isEmpty(Stack *stack){
+    if(stack->top == -1){
+        return true;
+    }
+
+    return false;
+}
+
+bool pop(Stack *stack){
+    //Check if the stack is empty
+    if(stack->top == -1){
+        return false;
+    }
+    (stack->top)--;
+    return true;
+}
+
+void freeStack(Stack *stack){
+    free(stack);
+}
